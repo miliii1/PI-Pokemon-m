0c5850ea0bc95602d1a44020d9cf31b680a1ab9f
miliii1 <milagrosjara26@gmail.com>
2021-08-17 15:33:57 -0300

    martes modificado

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom'
+import Home from './components/Home/index'
+
+function App() {
+  return (
+    hdsyug
+  );
+}
+
+export default App;
+
---
+++ client/src/actions/index.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import {
+    GET_POKEMONS,
+    GET_POKEMONS_DETAIL,
+    CREATE_NEW_POKEMON,
+    FILTER_POKEMON,
+    SEARCH_POKEMONS,
+    GET_TYPES
+
+    } from './constants'
+
+export function getPokemos(){
+    return async function (dispatch){
+        return await axios.get("http://localhost:3001/pokemons")
+        .then(response => {
+            dispatch({type: GET_POKEMONS, payload: response.data})
+        })
+    }
+}
+
+export function getPokemonDetail(id) {
+    return async function (dispatch){
+        axios.get(`http://localhost:3001/pokemons/${id}`)
+        .then(response => {
+            dispatch({type: GET_POKEMONS_DETAIL, payload:response.data[0]})
+        })
+    }
+}
+
+export function createNewPokemon(datos) {
+    return async function (dispatch){
+        const res = await axios.GET_POKEMONS_DETAIL('http://localhost:3001/pokemons', datos);
+        dispatch({type: CREATE_NEW_POKEMON, payload: res})
+    }
+}
+
+export function filterPokemon(datos){
+    return async function (dispatch){
+        dispatch({type: FILTER_POKEMON, payload: datos})
+    }
+}
+
+export function getTypes() {
+    return async (dispatch) => 
+    await axios
+    .get('https://localhost:3000/pokemons/type')
+    .then((res) => res.datos)
+    .then((type) = {
+        dispatch({type: GET_TYPES, payload: type})
+    })
+}
+
+export const searchPokemons = (name) => async (dispatch) => {
+    try{
+        const res = await axios.get('http://localhost:3000/pokemons/?name=' + name);
+        dispatch({type: SEARCH_POKEMONS, payload: res.datos}); 
+    } catch (error) {
+        console.log(error)
+    }
+}
