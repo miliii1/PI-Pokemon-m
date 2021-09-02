397d078644017ad909b7b28fbd375727b464017e
miliii1 <milagrosjara26@gmail.com>
2021-09-02 08:16:28 -0300

    listo

--- api/index.js
+++ api/index.js
@@ -22,23 +22,28 @@ const {conn, Type, Pokemon} = require('./src/db.js');
 const axios = require('axios');
 const {POKEMON_TYPE} = require('./src/utils/constants');
 
+//inicio el servidor, por ende llevo los types a la base de datos
+
 conn.sync({force: true}).then(() => {
 	server.listen(3001, () => {
 		console.log('Server is listening on port 3001');
-		axios.get('https://pokeapi.co/api/v2/pokemon/psyduck').then((r) =>
+		axios.get('https://pokeapi.co/api/v2/pokemon/zubat').then((a) =>
 			Pokemon.create({
-				name: r.data.name,
-				hp: r.data.stats[0].base_stat,
-				attack: r.data.stats[1].base_stat,
-				defense: r.data.stats[2].base_stat,
-				speed: r.data.stats[5].base_stat,
-				height: r.data.height,
-				weight: r.data.weight,
-				sprite: r.data.sprites.front_default,
+				name: a.data.name,
+				hp: a.data.stats[0].base_stat,
+				attack: a.data.stats[1].base_stat,
+				defense: a.data.stats[2].base_stat,
+				speed: a.data.stats[5].base_stat,
+				height: a.data.height,
+				weight: a.data.weight,
+				Types: a.types.map((a) => {
+					return {name: a.type.name};
+				}),
+				sprite: a.data.sprites.front_default,
 			})
 		);
-		axios.get(POKEMON_TYPE).then((r) => {
-			r.data.results.forEach((el) => Type.create({name: el.name}));
+		axios.get(POKEMON_TYPE).then((a) => {
+			a.data.results.forEach((el) => Type.create({name: el.name}));
 			console.log('The types were created successfully');
 		});
 	});
